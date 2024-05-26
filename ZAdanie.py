0a6a7ecbb07c1652c313f95f1bc8d12daee52573
Madejski98 <daniel.madejski98@gmail.com>
2024-05-26 13:23:49 +0200

    poprawka

--- ZAdanie.py
+++ ZAdanie.py
@@ -11,14 +11,34 @@ class Smartfon():
         self.model = model
         self.max_pamiec = max_pamiec
         self.pamiec = max_pamiec
+        self.stan = 'off'
 
     def zainstaluj_app(self, nazwa_app, pamiec_app):
-        if self.pamiec > pamiec_app:
-            self.aplikacje[nazwa_app] = pamiec_app
-            self.pamiec -= pamiec_app
-            print(self.aplikacje)
+        if self.stan == 'on':
+            if self.pamiec > pamiec_app:
+                self.aplikacje[nazwa_app] = pamiec_app
+                self.pamiec -= pamiec_app
+                print(self.aplikacje)
+            else:
+                print("Brak miejsca. Zwolnij miejce w pamięci!")
         else:
-            print("Brak miejsca. Zwolnij miejce w pamięci!")
+            print("Najpierw włącz telefon.")
+
+    def wlacz_tel(self):
+        if self.stan == 'off':
+            self.stan = 'on'
+        else:
+            print('Nie możesz włączyć włączony telefon!')
+
+    def wylacz_tel(self):
+        if self.stan == 'on':
+            self.stan = 'off'
+        else:
+            print('Nie możesz wyłączyć wyłączony telefon!')
 
 samsung = Smartfon('Samsung', 'S24', 16000)
+samsung.wlacz_tel()
+samsung.wlacz_tel()
 samsung.zainstaluj_app('Angry Birds', 1000)
+samsung.wylacz_tel()
+samsung.wylacz_tel()
